35d797c1a46a708774c7a76a9cd1e98a3e1a3cc5
Alice Fung <alice.o.fung@gmail.com>
2015-07-02 14:31:21 -0400

    add connect four file for spec testing

---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,26 @@
+=begin
+Game class
+  initialize
+  start_game
+  choose_opponent
+  play_against_AI?
+
+Play
+  initialize
+  get_input
+  move
+  check?
+  find_row
+
+Board
+  initialize
+  render
+  build_board
+  game_over?
+  winning_combo?
+  tie?
+  vertical_win?
+  horizontal_win?
+  diagonal_win?
+
+=end
